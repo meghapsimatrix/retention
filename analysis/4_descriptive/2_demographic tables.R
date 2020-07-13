6a69aa83156bd6f205a9e14faa761618c455fa34
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2020-07-13 16:27:51 -0500

    Janet update

--- analysis/4_descriptive/2_demographic tables.R
+++ analysis/4_descriptive/2_demographic tables.R
@@ -17,16 +17,36 @@ load("Revised Datasets/R/cohort_cert.RData")
 
 names(cohort_cert)
 cohort_cert$system ## QUESTION - how do I get the names of the rows in the 'system' variable?
+# do table here
+table(cohort_cert$system)
+
 str(cohort_cert$scorenum)
 
 #Grouping by and providing relative demographic proportions
 demog_tables <- cohort_cert %>%
   group_by(trad, ethnicity, gender) %>%
   summarize(n = n()) %>%
-  mutate(freq = n / sum(n)) %>%
-  summarize(mean_scorenum = mean(cohort_cert$scorenum), sd_scorenum = sd(cohort_cert$scorenum)) %>% #getting mean and sd
-  # of scorenum (i.e., certification score) for each of the groups
-  filter(system == UH) ### NEED TO WORK ON FILTERING COHORT_CERT BY SYSTEM AND UT AUSTIN BY ITSELF
+  mutate(freq = n / sum(n)) 
+
+# it's easier to do things one by one  - but you have the right idea 
+# if you group by trad and gender and ethncity- it is going to give freq of people who are femalemale and white and in trad route -
+# we normally don't care about joint dist like that
+# we want dist of gender by trad only 
+gender <- cohort_cert %>%
+  group_by(trad, gender) %>%
+  summarize(n = n()) %>%
+  mutate(freq = n / sum(n)) 
+
+ethnicity <- cohort_cert %>%
+  group_by(trad, gender) %>%
+  summarize(n = n()) %>%
+  mutate(freq = n / sum(n))
+
+
+score_num <- cohort_cert %>%
+  group_by(trad) %>%
+  summarize(m = mean(score_num),
+            sd = sd(score_num))
   
   
   
